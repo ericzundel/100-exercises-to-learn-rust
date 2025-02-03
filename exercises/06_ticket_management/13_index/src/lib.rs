46f04d70209734672b070d391705303e4be37ebd
Eric Z. Ayers <ericzundel@gmail.com>
2025-02-02 22:03:53 -0500

    Completed 6.13

--- exercises/06_ticket_management/13_index/src/lib.rs
+++ exercises/06_ticket_management/13_index/src/lib.rs
@@ -8,8 +8,25 @@ pub struct TicketStore {
     counter: u64,
 }
 
+use std::ops::Index;
+
+
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct TicketId(u64);
+
+impl Index<&TicketId> for TicketStore {
+    type Output = Ticket;
+
+    fn index(&self, index: &TicketId) -> &Self::Output {
+        for ticket in self.tickets.iter() {
+            if ticket.id == *index {
+                return ticket;
+            }
+        }
+        panic!("TicketId {:?} not found", *index);
+    }
+}
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ticket {
